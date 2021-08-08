3f963116d70fdb6172927d0a1331ada3245a6b6f
limnohsung <41315887+limnohsung@users.noreply.github.com>
2021-08-09 01:31:37 +0900

    업로드

---
+++ html/js/raw_source.js
@@ -0,0 +1,6 @@
+<script>
+    $(document).ready(function(){
+        jQuery(".btn_add_padding").click(function(){
+            confirm("취소 시 입력한 원재료 등록 정보는 저장되지 않으며, 영구히 삭제 됩니다. 취소하시겠습니까?");
+        });
+</script>
